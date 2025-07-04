4cbd108ec36044445d5f0418b6d7e4dfcbcf23e9
ROrozcoM <rafozco@gmail.com>
2025-07-04 10:02:36 +0200

    configuración correo

--- script.js
+++ script.js
@@ -43,3 +43,48 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 });
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = new FormData(form);
+  fetch(form.action, {
+    method: form.method,
+    body: data,
+    headers: { Accept: 'application/json' },
+  }).then(response => {
+    if (response.ok) {
+      alert("Mensaje enviado correctamente. ¡Gracias!");
+      form.reset();
+    } else {
+      alert("Error al enviar. Intenta más tarde.");
+    }
+  });
+  return false;
+}
+
+
+const formulario = document.getElementById("formulario");
+const mensaje = document.getElementById("form-mensaje");
+
+formulario.addEventListener("submit", async function(e) {
+  e.preventDefault();
+  const data = new FormData(formulario);
+
+  const res = await fetch(formulario.action, {
+    method: formulario.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  if (res.ok) {
+    formulario.reset();
+    mensaje.style.display = "block";
+  } else {
+    alert("Hubo un error. Intenta nuevamente más tarde.");
+  }
+});
+
+
+
